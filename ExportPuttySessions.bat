11f789b3fa77eb0e3f076d49ad5b92ddeffbefd4
Zeram <zeram3@gmail.com>
2019-01-07 12:32:23 -0400

    Create ExportPuttySessions.bat
    Create ExportPuttySessions.bat

---
+++ ExportPuttySessions.bat
@@ -0,0 +1,5 @@
+
+:: https://stackoverflow.com/questions/13023920/how-to-export-putty-sessions-list
+
+
+regedit /e "%USERPROFILE%\Desktop\putty-sessions.reg" HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions
